5d832f7c09fe9ee8eb5777bff4d77b92e21e9ef8
KushalGH <kushalseth1408@gmail.com>
2019-05-08 03:20:56 +0530

    added react

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware, compose} from "redux";
-import rootReducer from "./reducers/";
+import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
 
@@ -9,5 +10,5 @@ export default function configureStore(initialState) {
 
     return createStore( rootReducer, 
                         initialState, 
-                        composeEnhancer(applyMiddleware(reduxImmutableStateInvariant())));
+                        composeEnhancer(thunk, applyMiddleware(reduxImmutableStateInvariant())));
 }
